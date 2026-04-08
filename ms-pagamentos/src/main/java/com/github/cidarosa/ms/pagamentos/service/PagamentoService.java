41026c3de2a5957ebc103c697d75907d271c2a78
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-08 10:39:32 -0300

    feat ms-pagamentos: add crud faltante

--- ms-pagamentos/src/main/java/com/github/cidarosa/ms/pagamentos/service/PagamentoService.java
+++ ms-pagamentos/src/main/java/com/github/cidarosa/ms/pagamentos/service/PagamentoService.java
@@ -2,7 +2,10 @@ package com.github.cidarosa.ms.pagamentos.service;
 
 import com.github.cidarosa.ms.pagamentos.dto.PagamentoDTO;
 import com.github.cidarosa.ms.pagamentos.entities.Pagamento;
+import com.github.cidarosa.ms.pagamentos.entities.Status;
+import com.github.cidarosa.ms.pagamentos.exceptions.ResourceNotFoundException;
 import com.github.cidarosa.ms.pagamentos.repository.PagamentoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,5 +26,61 @@ public class PagamentoService {
         return pagamentos.stream()
                 .map(PagamentoDTO::new)
                 .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public PagamentoDTO findPagamentoById(Long id){
+
+        Pagamento pagamento = pagamentoRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Recurso não encontrado. ID: " + id)
+        );
+
+        return new PagamentoDTO(pagamento);
+    }
+
+    @Transactional
+    public PagamentoDTO save(PagamentoDTO pagamentoDTO){
+
+        Pagamento pagamento = new Pagamento();
+        mapDtoToPagamento(pagamentoDTO, pagamento);
+
+        pagamento.setStatus(Status.CRIADO);
+        pagamento = pagamentoRepository.save(pagamento);
+        return new PagamentoDTO(pagamento);
+    }
+
+    @Transactional
+    public PagamentoDTO update(Long id, PagamentoDTO pagamentoDTO){
+
+        try {
+            Pagamento pagamento = pagamentoRepository.getReferenceById(id);
+            mapDtoToPagamento(pagamentoDTO, pagamento);
+            pagamento.setStatus(pagamentoDTO.getStatus());
+            pagamento = pagamentoRepository.save(pagamento);
+            return new PagamentoDTO(pagamento);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Recurso não encontrado. ID: " + id);
+        }
+    }
+
+    @Transactional
+
+    public void delete(Long id){
+
+        if (!pagamentoRepository.existsById(id)){
+            throw new ResourceNotFoundException("Recurso não encontrado. ID: " + id);
+        }
+
+        pagamentoRepository.deleteById(id);
+    }
+
+    private void mapDtoToPagamento(PagamentoDTO pagamentoDTO, Pagamento pagamento) {
+
+        pagamento.setValor(pagamentoDTO.getValor());
+        pagamento.setNome(pagamentoDTO.getNome());
+        pagamento.setNumeroCartao(pagamentoDTO.getNumeroCartao());
+        pagamento.setValidade(pagamentoDTO.getValidade());
+        pagamento.setCodigoSeguranca(pagamentoDTO.getCodigoSeguranca());
+        pagamento.setPedidoId(pagamentoDTO.getPedidoId());
     }
 }
