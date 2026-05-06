63d72425504853898424d51e49588b3ff0ee9f49
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-06 08:45:33 -0300

    test: update 422 controllerIT

--- ms-pagamentos/src/test/java/com/github/cidarosa/ms/pagamentos/controller/PagamentoControllerIT.java
+++ ms-pagamentos/src/test/java/com/github/cidarosa/ms/pagamentos/controller/PagamentoControllerIT.java
@@ -148,7 +148,7 @@ public class PagamentoControllerIT {
     }
 
     @Test
-    void updatePagamentoShouldReturn422WhenInvalid() throws Exception{
+    void updatePagamentoShouldReturn422WhenInvalid() throws Exception {
 
         pagamento = Factory.createPagamento();
         pagamento.setNome(null);
@@ -159,13 +159,29 @@ public class PagamentoControllerIT {
         String jsonRequestBody = objectMapper.writeValueAsString(requestDTO);
 
         mockMvc.perform(put("/pagamentos/{id}", existingId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .content(jsonRequestBody))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(jsonRequestBody))
                 .andDo(print())
                 .andExpect(status().isUnprocessableEntity())
                 .andExpect(jsonPath("$.error").value("Dados inválidos"))
                 .andExpect(jsonPath("$.errors").isArray());
+
+    }
+
+    @Test
+    void updatePagamentoShouldReturn404WhenIdDoesNotExist() throws Exception {
+
+        pagamento = Factory.createPagamento();
+        PagamentoDTO requestDTO = new PagamentoDTO(pagamento);
+        String jsonRequestBody = objectMapper.writeValueAsString(requestDTO);
+
+        mockMvc.perform(put("/pagamentos/{id}", nonExistingId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(jsonRequestBody))
+                .andDo(print())
+                .andExpect(status().isNotFound());
 
     }
 
