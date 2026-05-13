d051afc9e48adfbf9b14771557dd010b1322c039
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-13 10:37:00 -0300

    feat: endpoint testar balanceamento

--- ms-pedidos/src/main/java/com/github/cidarosa/ms/pedidos/controller/PedidoController.java
+++ ms-pedidos/src/main/java/com/github/cidarosa/ms/pedidos/controller/PedidoController.java
@@ -4,6 +4,7 @@ import com.github.cidarosa.ms.pedidos.dto.PedidoDto;
 import com.github.cidarosa.ms.pedidos.service.PedidoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -17,6 +18,11 @@ public class PedidoController {
 
     @Autowired
     private PedidoService pedidoService;
+
+    @GetMapping("/port")
+    public String port(@Value("${local.server.port}") String porta){
+        return "Instância respondeu na porta: " + porta;
+    }
 
     @GetMapping
     private ResponseEntity<List<PedidoDto>> getAllPedidos(){
